83f7ef076ad0da43e9347cacbc670f988c25adad
Lucas Telles <lltellesss@gmail.com>
2023-12-18 16:58:41 -0300

    added updateFieldHandler function

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+type USerProps = {
+  data: {
+    name: string;
+    email: string;
+  };
+  updateFieldHandler: (key: string, value: string) => void;
+};
 
-const UserForm = () => {
-  return <div>UserForm</div>;
+const UserForm = ({ data, updateFieldHandler }: USerProps) => {
+  return (
+    <div>
+      <div className="form-control">
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite seu nome"
+          required
+          value={data.name || ""}
+          onChange={(e) => updateFieldHandler("name", e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Digite seu email"
+          required
+          value={data.email || ""}
+          onChange={(e) => updateFieldHandler("email", e.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default UserForm;
